b03b0e077cd72d5c1f3dccdccb7217bd721af7b3
lp6m <lp6m2014@gmail.com>
2017-10-10 15:20:18 +0900

    Chapter7-1, 7-2動いた

---
+++ blink_zq/blink_zq.sdk/led_test/src/led_test.c
@@ -0,0 +1,34 @@
+/* Copyright(C) 2016 Cobac.Net All Rights Reserved. */
+/* chapter: 第7章             */
+/* project: blink_zq          */
+/* outline: LED点滅プログラム */
+
+#include "xparameters.h"
+#include "xil_printf.h"
+#include "xil_types.h"
+#include <stdio.h>
+#define LED      *((volatile unsigned int*) (XPAR_GPIO_0_BASEADDR + 0x00))
+#define LED_ctrl *((volatile unsigned int*) (XPAR_GPIO_0_BASEADDR + 0x04))
+
+int main()
+{
+    int i, j;
+    LED_ctrl = 0x0; /* 全ビットを出力に設定 */
+    printf("Hello FPGA World!\n");
+    while(1) {
+        for ( i=0; i<6; i++ ) {
+            printf("i=%d\n", i);
+            switch ( i ) {
+                case  0: LED = 0x1; break;
+                case  1: LED = 0x2; break;
+                case  2: LED = 0x4; break;
+                case  3: LED = 0x8; break;
+                case  4: LED = 0x4; break;
+                case  5: LED = 0x2; break;
+                default: LED = 0x0;
+            }
+            for ( j=0; j<10000000; j++); /* 遅延を作成 */
+        }
+    }
+    return 0;
+}
---
+++ blink_zq/blink_zq.srcs/constrs_1/new/blink_zq.xdc
@@ -0,0 +1,14 @@
+#zynq Rev.D
+
+#chapter7 blink_zq
+set_property PACKAGE_PIN T22 [get_ports {LED[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[0]}]
+
+set_property PACKAGE_PIN T21 [get_ports {LED[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[1]}]
+
+set_property PACKAGE_PIN U22 [get_ports {LED[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[2]}]
+
+set_property PACKAGE_PIN U21 [get_ports {LED[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[3]}]
